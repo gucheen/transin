963048e0fbe1d222bf50a20163a9d9881494c295
gucheen <guchengf@gmail.com>
2025-02-21 10:28:44 +0800

    feat: deal with scale of image

--- web/src/components/image-region-marker/index.tsx
+++ web/src/components/image-region-marker/index.tsx
@@ -10,20 +10,21 @@ function ImageRegionMarker() {
   const isDrawing = useRef(false)
   const [startPoint, setStartPoint] = useState({ x: 0, y: 0 })
   const imageRef = useRef<HTMLImageElement>(null)
+  const scale = useRef(1)
 
   useEffect(() => {
     // 绘制选择框
     const drawSelect = () => {
       if (ctxRef.current && canvasRef.current && imageRef.current) {
         ctxRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
-        ctxRef.current.drawImage(imageRef.current, 0, 0, imageRef.current.width, imageRef.current.height)
+        ctxRef.current.drawImage(imageRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height)
         if (currentPosition.x && currentPosition.y) {
           const w = currentPosition.x - startPoint.x
           const h = currentPosition.y - startPoint.y
           if (w && h) {
             ctxRef.current.strokeStyle = '#38f'
             ctxRef.current.lineWidth = 2
-            ctxRef.current.strokeRect(startPoint.x, startPoint.y, currentPosition.x - startPoint.x, currentPosition.y - startPoint.y)
+            ctxRef.current.strokeRect(startPoint.x, startPoint.y, w, h)
           }
         }
       }
@@ -39,6 +40,26 @@ function ImageRegionMarker() {
     const ctx = canvas.getContext('2d')
     if (!ctx) return
 
+    const getInitialSelection = () => {
+      socket.emit('settings:get-ocr-recognize-area', (response: {
+        left: number,
+        top: number,
+        width: number,
+        height: number,
+      }) => {
+        if (response) {
+          setStartPoint({
+            x: response.left,
+            y: response.top,
+          })
+          setCurrentPosition({
+            x: response.left + response.width,
+            y: response.top + response.height,
+          })
+        }
+      })
+    }
+
     ctxRef.current = ctx
 
     // 加载图片
@@ -48,13 +69,17 @@ function ImageRegionMarker() {
     img.onload = () => {
       canvas.width = img.width
       canvas.height = img.height
-      ctx.drawImage(img, 0, 0, img.width, img.height)
+      const { width, height } = canvas.getBoundingClientRect()
+      const trueScale = Math.min(img.width / width, img.height / height)
+      scale.current = trueScale
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+      getInitialSelection()
     }
 
     const onMouseDown = (e: MouseEvent) => {
       const rect = canvas.getBoundingClientRect()
-      const x = e.clientX - rect.left
-      const y = e.clientY - rect.top
+      const x = (e.clientX - rect.left) * scale.current
+      const y = (e.clientY - rect.top) * scale.current
       isDrawing.current = true
       setStartPoint({ x, y })
       setCurrentPosition({ x, y })
@@ -66,8 +91,8 @@ function ImageRegionMarker() {
     const onMouseMove = (e: MouseEvent) => {
       if (!isDrawing.current) return
       const rect = canvas.getBoundingClientRect()
-      const x = e.clientX - rect.left
-      const y = e.clientY - rect.top
+      const x = (e.clientX - rect.left) * scale.current
+      const y = (e.clientY - rect.top) * scale.current
       setCurrentPosition({
         x,
         y,
@@ -84,24 +109,6 @@ function ImageRegionMarker() {
     // 处理鼠标释放事件
     canvas.addEventListener('mouseup', onMouseUp)
 
-    socket.emit('settings:get-ocr-recognize-area', (response: {
-      left: number,
-      top: number,
-      width: number,
-      height: number,
-    }) => {
-      if (response) {
-        setStartPoint({
-          x: response.left,
-          y: response.top,
-        })
-        setCurrentPosition({
-          x: response.left + response.width,
-          y: response.top + response.height,
-        })
-      }
-    })
-
     return () => {
       canvas.removeEventListener('mousedown', onMouseDown)
       canvas.removeEventListener('mousemove', onMouseMove)
@@ -112,10 +119,10 @@ function ImageRegionMarker() {
 
   const submitAreaInfo = () => {
     const payload = {
-      left: Math.min(startPoint.x, currentPosition.x),
-      top: Math.min(startPoint.y, currentPosition.y),
-      width: Math.abs(currentPosition.x - startPoint.x),
-      height: Math.abs(currentPosition.y - startPoint.y),
+      left: ~~Math.min(startPoint.x, currentPosition.x),
+      top: ~~Math.min(startPoint.y, currentPosition.y),
+      width: ~~Math.abs(currentPosition.x - startPoint.x),
+      height: ~~Math.abs(currentPosition.y - startPoint.y),
     }
     console.log(payload)
     if (payload.width !== 0 && payload.height !== 0) {
@@ -128,13 +135,21 @@ function ImageRegionMarker() {
       <div className='canvas-wrapper'>
       <canvas ref={canvasRef} />
       </div>
+      <div className="side-info">
       <div className='selection-info'>
-        <span className='coordinate-text'>
-        x: <span className='coordinate-x'>{startPoint.x}</span>, y: <span className='coordinate-y'>{startPoint.y}</span>, width: <span className='dimension-width'>{currentPosition.x - startPoint.x}</span>, height: <span className='dimension-height'>{currentPosition.y - startPoint.y}</span>
-        </span>
+        <div className='coordinate-text'>
+        x: <span className='coordinate-x'>{startPoint.x}</span>, y: <span className='coordinate-y'>{startPoint.y}</span>
+        <div>
+        width: <span className='dimension-width'>{currentPosition.x - startPoint.x}</span>
+        </div>
+        <div>
+        height: <span className='dimension-height'>{currentPosition.y - startPoint.y}</span>
+        </div>
+        </div>
       </div>
 
       <button className='confirm-button' onClick={submitAreaInfo}>提交区域信息</button>
+      </div>
     </div>
   )
 }
